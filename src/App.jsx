512a14c0f84cbbd3d42fc842ef353ddac59db114
vivaldev <valtteri.virtanen.dev@gmail.com>
2023-10-07 19:48:09 +0300

    setTodos

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,59 @@
-import './styles.css';
+import { useState } from "react";
+import "./styles.css";
 
 export default function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setTodos((currentTodos) => {
+      return [
+        ...todos,
+        {
+          id: todos.length + 1,
+          text: newItem,
+          completed: false,
+        },
+      ];
+    });
+  }
+
+  console.log(todos);
+
   return (
     <>
-  <form className='new-item-form'>
-    <div className='form-row'>
-      <label htmlFor='item'>New Item</label>
-      <input type='text' issd='item' />
-    </div>
-    <button className='btn'>Add</button>
-  </form>
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            issd="item"
+          />
+        </div>
+        <button className="btn">Add</button>
+      </form>
 
-  <h1 className='header'>Todo List</h1>
-  <ul className='list'>
-    <li>
-      <label>
-        <input type='checkbox' className='checkbox' />
-        Item 1
-      </label>
-      <button className='btn btn-danger'>Delete</button>
-    </li>
-    <li>
-      <label>
-        <input type='checkbox' className='checkbox' />
-        Item 2
-      </label>
-      <button className='btn btn-danger'>Delete</button>
-    </li>
-  </ul>
-  </>
-  )
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" className="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" className="checkbox" />
+            Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
 }
